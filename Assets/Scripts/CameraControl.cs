5a3b43746db4d3fdb438b38e8737f15458b9d42f
Exaii <Adrian.v1@gmx.de>
2025-06-17 21:54:11 +0200

    Updated camera system to include skys
    +sky shader
    +card texture
    +distortion textures
    +sky's the limit

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -10,6 +10,9 @@ public class CameraControl : MonoBehaviour
     public Vector2 mouseDeltaModifier = new Vector2(0.05f, 0.2f);
     public bool click;
 
+    public Vector3 heightVelocity;
+    public Vector3 rotationalVelocity;
+
     public Vector3 mouseInputLast;
     public Vector3 mouseInputCurrent;
     public Vector3 mouseDelta;
@@ -24,9 +27,23 @@ public class CameraControl : MonoBehaviour
         float active = click ? 1 : 0;
         
         float deltaHeightChange = Input.mouseScrollDelta.y + mouseDelta.y * active * mouseDeltaModifier.y;
-        cameraHeight.position = new Vector3(cameraHeight.position.x, Mathf.Clamp(cameraHeight.position.y + deltaHeightChange, 0, maxHeight), cameraHeight.position.z);
+
+        heightVelocity = new Vector3(
+            Mathf.Lerp(heightVelocity.x, deltaHeightChange, heightVelocity.z * Time.deltaTime), 
+            heightVelocity.y, 
+            heightVelocity.z);
+
+        cameraHeight.position = new Vector3(cameraHeight.position.x, Mathf.Clamp(cameraHeight.position.y + heightVelocity.x, 0, maxHeight), cameraHeight.position.z);
 
         float deltaCameraRotationChange = mouseDelta.x * active * mouseDeltaModifier.x;
-        cameraRotation.Rotate(0, deltaCameraRotationChange, 0);
+        
+        rotationalVelocity = new Vector3(
+            Mathf.Lerp(rotationalVelocity.x, deltaCameraRotationChange, rotationalVelocity.z * Time.deltaTime), 
+            rotationalVelocity.y, 
+            rotationalVelocity.z);
+        
+        cameraRotation.Rotate(0, rotationalVelocity.x, 0);
     }
+
+
 }
